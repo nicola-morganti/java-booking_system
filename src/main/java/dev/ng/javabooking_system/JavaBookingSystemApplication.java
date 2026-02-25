ab701f918565bcb0dd784af89c9000d7406e3f7e
nicola-morganti <nicola.morganti@bluewin.ch>
2026-02-25 11:51:13 +0100

    Initial Commit

---
+++ src/main/java/dev/ng/javabooking_system/JavaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.ng.javabooking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ng/javabooking_system/JavaBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ng.javabooking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
