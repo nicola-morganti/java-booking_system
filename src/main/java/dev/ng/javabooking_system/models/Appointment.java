9c4d5762ec50f3fb8a045b6b0bd77e24789c00b4
nicola-morganti <nicola.morganti@bluewin.ch>
2026-03-04 10:29:07 +0100

    Added some files 😔

---
+++ src/main/java/dev/ng/javabooking_system/models/Appointment.java
@@ -0,0 +1,4 @@
+package dev.ng.javabooking_system.models;
+
+public class Appointment {
+}
---
+++ src/main/java/dev/ng/javabooking_system/repositories/AppointmentRepository.java
@@ -0,0 +1,4 @@
+package dev.ng.javabooking_system.repositories;
+
+public interface AppointmentRepository {
+}
