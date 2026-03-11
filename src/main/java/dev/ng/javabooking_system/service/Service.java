e5dd634aaf0c29b3c064c6c3d468f223b4733d88
Gonçalo Pereira <goncalo.dinis.spe@gmail.com>
2026-03-11 09:02:50 +0100

    changes to service

--- src/main/java/dev/ng/javabooking_system/service/Service.java
+++ src/main/java/dev/ng/javabooking_system/service/Service.java
@@ -1,4 +1,22 @@
 package dev.ng.javabooking_system.service;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "services")
 public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer duration;
+
 }
